31fa3682838c6c162b711a19a6636b55d821bd77
Miroslav Kopecký <miroslav.s.kopecky@gmail.com>
2023-10-14 01:15:37 +0200

    added page presets, first page steps

---
+++ assets/js/plot.js
@@ -0,0 +1,127 @@
+var prevEvent, currentEvent;
+var speedXData = [];
+var speedYData = [];
+var timeData = [];
+var chart;
+var maxSpeedX = 0;
+var maxSpeedY = 0;
+
+function handleInputEvent(event) {
+    currentEvent = event;
+}
+
+document.documentElement.onmousemove = handleInputEvent;
+document.documentElement.addEventListener('touchmove', function (event) {
+    event.preventDefault();
+    handleInputEvent(event.touches[0]);
+});
+
+setInterval(function () {
+    if (prevEvent && currentEvent) {
+        var speedX = (currentEvent.screenX - prevEvent.screenX) * 10;
+        var speedY = (prevEvent.screenY - currentEvent.screenY) * 10;
+
+        document.getElementById("speedX").innerText = Math.round(speedX);
+        document.getElementById("speedY").innerText = Math.round(speedY);
+
+        document.getElementById("maxSpeedX").innerText = Math.round(
+            speedX > maxSpeedX ? (maxSpeedX = speedX) : maxSpeedX
+        );
+
+        document.getElementById("maxSpeedY").innerText = Math.round(
+            speedY > maxSpeedY ? (maxSpeedY = speedY) : maxSpeedY
+        );
+
+        if (speedXData.length > 100) {
+            speedXData.shift();
+            speedYData.shift();
+            timeData.shift();
+        }
+
+        speedXData.push(speedX);
+        speedYData.push(speedY);
+        timeData.push(new Date().toLocaleTimeString());
+
+        updateChart();
+    }
+
+    prevEvent = currentEvent;
+}, 150);
+
+function updateChart() {
+    if (!chart) {
+        var ctx = document.getElementById("speedChart").getContext("2d");
+        chart = new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: timeData,
+                datasets: [
+                    {
+                        label: "Speed X",
+                        data: speedXData,
+                        borderColor: "#f0bb74",
+                        borderWidth: 3,
+                        fill: false,
+                        lineTension: 0.3,
+                        fill: "start",
+                        backgroundColor: "rgba(240, 187, 116, 0.05)",
+                    },
+                    {
+                        label: "Speed Y",
+                        data: speedYData,
+                        borderColor: "#5990ff",
+                        borderWidth: 2,
+                        fill: false,
+                        lineTension: 0.3,
+                        fill: "start",
+                        backgroundColor: "rgba(89, 144, 255, 0.05)"
+                    },
+                ],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+                plugins: {
+                    legend: {
+                        display: false,
+                    },
+                },
+                elements: {
+                    point: {
+                        radius: 0,
+                    }
+                },
+                scales: {
+                    x: {
+                        ticks: {
+                            display: false,
+                        },
+                        grid: {
+                            display: false,
+                        },
+                        border: {
+                            display: false,
+                        }
+                    },
+                    y: {
+                        ticks: {
+                            display: false,
+                        },
+                        grid: {
+                            color: "#eaf9ff",
+                        },
+                        border: {
+                            display: false,
+                            dash: [10, 10]
+                        }
+                    },
+                },
+            },
+        });
+    } else {
+        chart.data.labels = timeData;
+        chart.data.datasets[0].data = speedXData;
+        chart.data.datasets[1].data = speedYData;
+        chart.update();
+    }
+}
---
+++ assets/js/script.js
@@ -0,0 +1,285 @@
+(function () {
+    "use strict";
+
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
+
+    /**
+     * Easy on scroll event listener 
+     */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+    /**
+     * Navbar links active state on scroll
+     */
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 250
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+                if (navbarlink.className.includes("button-info")) {
+                    navbarlink.classList.remove('active');
+                }
+            } else {
+                navbarlink.classList.remove('active')
+            }
+        })
+        // if (isScrolledToBottom()) {
+        //   navbarlinks[2].classList.remove('active')
+        //   navbarlinks[4].classList.add('active')
+        // }
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+
+    /**
+     * Scrolls to an element with header offset
+     */
+    const scrollto = (el) => {
+        let header = select('#header')
+        let offset = header.offsetHeight
+
+        if (!header.classList.contains('header-scrolled')) {
+            offset -= 30
+        }
+
+        let elementPos = select(el).offsetTop
+        window.scrollTo({
+            top: elementPos - offset,
+            behavior: 'smooth'
+        })
+    }
+
+    /**
+     * Toggle .header-scrolled class to #header when page is scrolled
+     */
+    let selectHeader = select('#header')
+    let scrolled = false;
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 270) {
+                document.getElementById('logo-header').classList.add('show');
+                document.getElementById('logo-title-image').classList.add('hide');
+                document.getElementById('logo-header').classList.remove('hide');
+                document.getElementById('logo-title-image').classList.remove('show');
+            }
+            else {
+                document.getElementById('logo-header').classList.add('hide');
+                document.getElementById('logo-title-image').classList.add('show');
+                document.getElementById('logo-header').classList.remove('show');
+                document.getElementById('logo-title-image').classList.remove('hide');
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+
+    /**
+     * Back to top button
+     */
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 200) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
+
+    /**
+     * Mobile nav toggle
+     */
+    on('click', '.mobile-nav-toggle', function (e) {
+        select('#navbar').classList.toggle('navbar-mobile')
+        this.classList.toggle('bi-list')
+        this.classList.toggle('bi-x')
+    })
+
+    /**
+     * Mobile nav dropdowns activate
+     */
+    on('click', '.navbar .dropdown > a', function (e) {
+        if (select('#navbar').classList.contains('navbar-mobile')) {
+            e.preventDefault()
+            this.nextElementSibling.classList.toggle('dropdown-active')
+        }
+    }, true)
+
+    /**
+     * Scrool with ofset on links with a class name .scrollto
+     */
+    on('click', '.scrollto', function (e) {
+        if (select(this.hash)) {
+            e.preventDefault()
+
+            let navbar = select('#navbar')
+            if (navbar.classList.contains('navbar-mobile')) {
+                mobileMenu();
+                navbar.classList.remove('navbar-mobile')
+                let navbarToggle = select('.mobile-nav-toggle')
+                navbarToggle.classList.toggle('bi-list')
+                navbarToggle.classList.toggle('bi-x')
+            }
+            scrollto(this.hash)
+        }
+    }, true)
+
+    /**
+     * Skills animation
+     */
+    let skilsContent = select('.skills-content');
+    if (skilsContent) {
+        new Waypoint({
+            element: skilsContent,
+            offset: '80%',
+            handler: function (direction) {
+                let progress = select('.progress .progress-bar', true);
+                progress.forEach((el) => {
+                    el.style.width = el.getAttribute('aria-valuenow') + '%'
+                });
+            }
+        })
+    }
+
+    /**
+     * Testimonials slider
+     */
+    new Swiper('.testimonials-carousel', {
+        speed: 400,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        }
+    });
+
+    /**
+     * Porfolio isotope and filter
+     */
+    window.addEventListener('load', () => {
+        let portfolioContainer = select('.portfolio-container');
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+                itemSelector: '.portfolio-wrap',
+                layoutMode: 'fitRows'
+            });
+
+            let portfolioFilters = select('#portfolio-flters li', true);
+
+            on('click', '#portfolio-flters li', function (e) {
+                e.preventDefault();
+                portfolioFilters.forEach(function (el) {
+                    el.classList.remove('filter-active');
+                });
+                this.classList.add('filter-active');
+
+                portfolioIsotope.arrange({
+                    filter: this.getAttribute('data-filter')
+                });
+                portfolioIsotope.on('arrangeComplete', function () {
+                    AOS.refresh()
+                });
+            }, true);
+        }
+
+    });
+
+    /**
+     * Initiate portfolio lightbox 
+     */
+    const portfolioLightbox = GLightbox({
+        selector: '.portfolio-lightbox'
+    });
+
+    /**
+     * Portfolio details slider
+     */
+    new Swiper('.portfolio-details-slider', {
+        speed: 400,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        }
+    });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+})()
+
+// Function to check if an element is at least 2/3 into the viewport
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    return rect.top <= windowHeight*(3/4);
+}
+
+
+var app = document.getElementById('typed-text');
+
+var customNodeCreator = function(character) {
+  return document.createTextNode(character);
+}
+
+var typewriter = new Typewriter(app, {
+  loop: false,
+  delay: 40,
+  onCreateTextNode: customNodeCreator,
+});
+
+typewriter
+  .typeString('We <b>provide data converters</b> for telecommunication, satellite, radar and other applications where extreme performance, high energy efficiency or low-cost (or all of these combined) matter.')
+  .pauseFor(300)
+  .start();
